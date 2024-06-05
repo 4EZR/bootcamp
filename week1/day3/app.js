01040f433a5692ff44dfe3931e3d1c35ed1d9eb6
4EZR <juandreas.ezarfelix@gmail.com>
2024-06-05 19:15:38 +0700

    npm i validator + gitignore implementation

--- week1/day3/app.js
+++ week1/day3/app.js
@@ -1,1 +1,8 @@
+// ! First
 console.log("hello world")
+
+// ! Second Validator 
+const validator = require('validator');
+
+console.log(validator.isEmail('j@g.com'))
+console.log(validator.isMobilePhone('081234567890', 'en-US'));
