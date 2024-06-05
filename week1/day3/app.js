1b357a6b5be219f22d7265fc2e315fbb655a0110
4EZR <juandreas.ezarfelix@gmail.com>
2024-06-05 19:20:47 +0700

    nodemon

--- week1/day3/app.js
+++ week1/day3/app.js
@@ -6,3 +6,8 @@ const validator = require('validator');
 
 console.log(validator.isEmail('j@g.com'))
 console.log(validator.isMobilePhone('081234567890', 'en-US'));
+
+// ! third Nodemon
+// run npm install -g nodemon
+// then run nodemon app.js
+
