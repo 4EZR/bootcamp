b00caba76a7cdea217ed35820357fd3862b047d8
4EZR <juandreas.ezarfelix@gmail.com>
2024-06-10 23:26:59 +0700

    asdasd

--- week1/day6/app.js
+++ week1/day6/app.js
@@ -17,6 +17,10 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
 });
 
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
